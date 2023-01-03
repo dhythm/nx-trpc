1a1f017ae8cee9d8dbfef8138abcdae9bfebc336
dhythm <yuta.okada.20@gmail.com>
2023-01-03 21:06:38 +0900

    use httpLink instead of httpBatchLink

--- libs/trpc-client/src/lib/client.ts
+++ libs/trpc-client/src/lib/client.ts
@@ -1,13 +1,16 @@
 import type { AppRouter } from '@trpc-server';
-import { httpBatchLink } from '@trpc/client';
+import { httpLink } from '@trpc/client';
 import { createTRPCReact } from '@trpc/react-query';
 
 export const trpc = createTRPCReact<AppRouter>();
 
 export const trpcClient = trpc.createClient({
   links: [
-    httpBatchLink({
+    httpLink({
       url: 'http://localhost:3000/trpc',
     }),
+    // httpBatchLink({
+    //   url: 'http://localhost:3000/trpc',
+    // }),
   ],
 });
