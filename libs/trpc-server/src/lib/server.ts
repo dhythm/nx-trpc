ea944d18ea88aa4030d749c7bca21d097361d393
dhythm <yuta.okada.20@gmail.com>
2023-01-03 19:18:07 +0900

    split logic from server.ts to init.ts and user.ts

--- libs/trpc-server/src/lib/server.ts
+++ libs/trpc-server/src/lib/server.ts
@@ -1,31 +1,10 @@
-import { db } from '@db';
-import { initTRPC } from '@trpc/server';
-import { z } from 'zod';
-
-const t = initTRPC.create();
+import { t } from './init';
+import { createUser, getUser, getUsers } from './user';
 
 export const appRouter = t.router({
-  getUsers: t.procedure.query(async () => {
-    const users = await db.user.findMany();
-    return users;
-  }),
-  getUser: t.procedure.input(z.string()).query(async (req) => {
-    const id = req.input;
-    const user = await db.user.findFirst({ where: { id } });
-    return user;
-  }),
-  createUser: t.procedure
-    .input(z.object({ email: z.string().email(), name: z.string() }))
-    .mutation(async (req) => {
-      const { email, name } = req.input;
-      const user = await db.user.create({
-        data: {
-          email,
-          name,
-        },
-      });
-      return user;
-    }),
+  getUsers,
+  getUser,
+  createUser,
 });
 
 export type AppRouter = typeof appRouter;
