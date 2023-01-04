3f1333a4b927660703da28eee9918b88fe9cdbcb
dhythm <yuta.okada.20@gmail.com>
2023-01-04 22:54:10 +0900

    use msw with NODE_ENV in browser instead of import '@mock/client'

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,8 +1,14 @@
-import '@mock/client';
+// import '@mock/client';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 import App from './app/app';
+
+if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { worker } = require('@mock/client');
+  worker.start();
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
